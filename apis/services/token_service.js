20ec1c664c6f35933cbc21106f192f590f2be812
Phong Hồ <phonghh@unit.com.vn>
2022-11-03 21:36:32 +0700

    no message

--- apis/services/token_service.js
+++ apis/services/token_service.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken')
-const moment = require('moment')
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
 
 /**
  * Generate token
@@ -11,95 +11,93 @@ const moment = require('moment')
  */
 
 const generateTokenForSendVerificationEmail = (userBody) => {
-    const payload = {
-        firstName: userBody.firstName,
-        lastName: userBody.lastName,
-        fullName: userBody.fullName,
-        email: userBody.email,
-        password: userBody.password,
-        birthday: userBody.birthday,
-        sex: userBody.sex,
-        mobile: userBody.mobile
-    }
-    return jwt.sign(payload, process.env.PASSPORT_JWT_ACCOUNT_ACTIVATION, {
-        expiresIn: '5m',
-    })
-}
+  const payload = {
+    firstName: userBody.firstName,
+    lastName: userBody.lastName,
+    fullName: userBody.fullName,
+    email: userBody.email,
+    password: userBody.password,
+    birthday: userBody.birthday,
+    sex: userBody.sex,
+    mobile: userBody.mobile,
+  };
+  return jwt.sign(payload, process.env.PASSPORT_JWT_ACCOUNT_ACTIVATION, {
+    expiresIn: '5m',
+  });
+};
 
 const generateAccessToken = (id) => {
-    const payload = {
-        id: id
-    }
-    return jwt.sign(payload, process.env.PASSPORT_JWT_ACCESS_TOKEN, {
-        expiresIn: '1d',
-    })
-}
+  const payload = {
+    id: id,
+  };
+  return jwt.sign(payload, process.env.PASSPORT_JWT_ACCESS_TOKEN, {
+    expiresIn: '1d',
+  });
+};
 
 const generateRefreshToken = (id) => {
-    const payload = {
-        id: id
-    }
-    return jwt.sign(payload, process.env.PASSPORT_JWT_REFRESH_TOKEN, {
-        expiresIn: '7d',
-    })
-}
+  const payload = {
+    id: id,
+  };
+  return jwt.sign(payload, process.env.PASSPORT_JWT_REFRESH_TOKEN, {
+    expiresIn: '7d',
+  });
+};
 
 const generateResetPasswordToken = (email) => {
-    const resetPasswordToken = jwt.sign(
-        {
-            email,
-        },
-        process.env.PASSPORT_JWT_RESET_PASSWORD,
-        {
-            expiresIn: '5m',
-        }
-    )
-    return resetPasswordToken
-}
+  const resetPasswordToken = jwt.sign(
+    {
+      email,
+    },
+    process.env.PASSPORT_JWT_RESET_PASSWORD,
+    {
+      expiresIn: '5m',
+    }
+  );
+  return resetPasswordToken;
+};
 
 const checkAccessTokenAndRefreshToken = (accessToken, refreshToken) => {
+  try {
+    jwt.verify(accessToken, process.env.PASSPORT_JWT_ACCESS_TOKEN);
+    const token = {
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    };
+    return token;
+  } catch (err) {
     try {
-        jwt.verify(accessToken, process.env.PASSPORT_JWT_ACCESS_TOKEN)
-        const token = {
-            accessToken: accessToken,
-            refreshToken: refreshToken
-        }
-        return token
+      jwt.verify(refreshToken, process.env.PASSPORT_JWT_REFRESH_TOKEN);
+      const { id } = jwt.decode(refreshToken);
+      const newAccessToken = generateAccessToken(id);
+      const newRefreshToken = generateRefreshToken(id);
+
+      const token = {
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken,
+      };
+      return token;
     } catch (err) {
-        try {
-            jwt.verify(refreshToken, process.env.PASSPORT_JWT_REFRESH_TOKEN)
-            const { id } = jwt.decode(refreshToken)
-            const newAccessToken = generateAccessToken(id)
-            const newRefreshToken = generateRefreshToken(id)
-
-            const token = {
-                accessToken: newAccessToken,
-                refreshToken: newRefreshToken
-            }
-            return token
-        } catch (err) {
-            throw new CustomError(500, err.message)
-        }
+      throw new CustomError(500, err.message);
     }
+  }
+};
 
-}
-
-const getUserIDByRefreshToken= (refreshToken) => {
-    try {
-        jwt.verify(refreshToken, process.env.PASSPORT_JWT_REFRESH_TOKEN)
-        const { id } = jwt.decode(refreshToken)
-        return id.id
-    } catch (err) {
-        throw new CustomError(500, err.message)
-    }
-
-}
+const getUserIDByRefreshToken = (refreshToken) => {
+  try {
+    jwt.verify(refreshToken, process.env.PASSPORT_JWT_REFRESH_TOKEN);
+    const { id } = jwt.decode(refreshToken);
+    return id.id;
+  } catch (err) {
+    throw new CustomError(500, err.message);
+  }
+};
 
 module.exports = {
-    generateTokenForSendVerificationEmail,
-    generateAccessToken,
-    generateRefreshToken,
-    generateResetPasswordToken,
-    checkAccessTokenAndRefreshToken,
-    getUserIDByRefreshToken,
-}
+  generateTokenForSendVerificationEmail,
+  generateAccessToken,
+  generateRefreshToken,
+  generateResetPasswordToken,
+  checkAccessTokenAndRefreshToken,
+  getUserIDByRefreshToken,
+};
--- server.js
+++ server.js
@@ -1,61 +1,60 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')   
-const passport = require('passport')
-const SocketServer = require('./socketServer')
-const { ExpressPeerServer } = require('peer')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const SocketServer = require('./socketServer');
+const { ExpressPeerServer } = require('peer');
+const path = require('path');
 
-const {jwtStrategy} = require('./apis/plugins/passport')
+const { jwtStrategy } = require('./apis/plugins/passport');
 
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
-
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 // Routes
-app.use('/api', require('./apis/routes/index'))
+app.use('/api', require('./apis/routes/index'));
 
 //Passport
-app.use(passport.initialize())
-passport.use('jwt', jwtStrategy)
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
 
 // Socket
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-io.on('connection', socket => {
-    console.log(socket.id + "connected")
-    SocketServer(socket)
-})
+io.on('connection', (socket) => {
+  console.log(socket.id + 'connected');
+  SocketServer(socket);
+});
 
 // Create peer server
-ExpressPeerServer(http, { path: '/' })
+ExpressPeerServer(http, { path: '/' });
 
-
-
-const URI = process.env.DB_CONNECTION
-mongoose.connect(URI, {
+const URI = process.env.DB_CONNECTION;
+mongoose.connect(
+  URI,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, err => {
-    if(err) throw err;
-    console.log('Connected to mongodb')
-})
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log('Connected to mongodb');
+  }
+);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 5050;
 http.listen(port, () => {
-    console.log('Server is running on port', port)
-})
+  console.log('Server is running on port', port);
+});
